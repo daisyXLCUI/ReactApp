ce2176828613a8faf354a75755eec53bf51b652e
daisyXLCUI <645401126@qq.com>
2018-01-09 23:32:48 +0800

    update code

---
+++ src/commont/header.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+/**
+ * 公共头部
+ *
+ * @export
+ * @class Header
+ * @extends {Component}
+ */
+
+
+class Header extends Component {  //头部标题
+    render() {
+        return(
+            <h1> 头部 </h1>
+        )
+    }
+}
+
+export default Header;
+
